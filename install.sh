5b48bbf1ef348a9d5c2abf66eb0f3f4f6e53e998
nesges <thomas@nesges.eu>
2022-11-11 15:10:29 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ echo ''
 echo '!! PROCEED AT OWN RISK !!'
 echo ''
 echo 'This is more a less just a notepad of what I did on my personal installation'
-echo '(Ubuntu 22) to make moa work. I have no idea if this BREAKS any other system.'
-echo 'Use this skript at your own RISK.'
+echo '(Ubuntu 22) to make moa work. I have literally no idea if this could BREAK'
+echo 'any other system. You may want to review the skript prior to running it and'
+echo 'by all means: Use it at your own RISK! I cannot support you, if anything goes'
+eche 'sideways.'
 echo ''
 echo '!! PROCEED AT OWN RISK !!'
 echo ''
@@ -21,10 +23,10 @@ ask() {
 execute() {
     echo ''
     echo I am going to execute:
-    echo $1
+    echo $@
     echo ''
     ask
-    $1
+    $@
 }
 
 
@@ -34,13 +36,19 @@ echo ''
 echo '[ APT ]'
 echo ''
 echo 'Installing required packages'
-execute "apt update"
-execute "apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python3-dev"
+execute "sudo apt update"
+execute "sudo apt install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python3-dev"
 
 echo ''
 echo '[ PYENV ]'
 echo ''
 echo 'Installing pyenv'
+echo ''
+echo I am going to execute:
+echo 'curl https://pyenv.run | bash'
+echo ''
+ask
+curl https://pyenv.run | bash
 echo '# pyenv' >> ~/.bashrc
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
@@ -52,24 +60,29 @@ echo ''
 echo '[ PYTHON ]'
 echo ''
 echo 'Install python 3.7.15'
-execute "~/.pyenv/bin/pyenv install 3.7.15"
-execute "~/.pyenv/bin/pyenv local 3.7.15"
+execute ~/.pyenv/bin/pyenv install 3.7.15
+execute ~/.pyenv/bin/pyenv local 3.7.15
 
 echo ''
 echo '[ PIPENV ]'
 echo ''
 echo 'Install pipenv'
-execute "~/.pyenv/shims/pipenv pip3 install pipenv"
-execute "PIPENV_VENV_IN_PROJECT=1 ~/.pyenv/shims/pipenv install"
+execute pip3 install pipenv
+execute PIPENV_VENV_IN_PROJECT=1 pipenv install
 
 echo ''
 echo '[ CONFIG ]'
 echo ''
 echo 'Copy Configfile'
-execute "cp config.py.sample config.py"
+execute cp config.py.sample config.py
 
 echo ''
 echo '[ DATABASE ]'
 echo ''
 echo 'Create SQLite database in moa.db'
-execute "MOA_CONFIG=DevelopmentConfig pipenv run python -m moa.models"
+execute MOA_CONFIG=DevelopmentConfig pipenv run python -m moa.models
+
+echo ''
+echo 'I am done here. Next you can run the server with ./run-dev.sh and the workers'
+echo 'with ./run-worker.sh. You may want to run d3xpost as a systemd service. Use'
+echo 'd3xpost-service as a template for that. Good luck! :-)'
